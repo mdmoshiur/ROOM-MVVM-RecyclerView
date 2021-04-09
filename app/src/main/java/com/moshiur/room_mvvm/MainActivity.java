6c199f06ddf61a84c1b8d98c4294954fd814c01f
mdmoshiur <moshiur1176@gmail.com>
2021-04-10 03:32:17 +0600

    Reference of viewmodel and activity fragment are added in main activity
    also dataset initialize with some values

--- app/src/main/java/com/moshiur/room_mvvm/MainActivity.java
+++ app/src/main/java/com/moshiur/room_mvvm/MainActivity.java
@@ -1,14 +1,33 @@
 package com.moshiur.room_mvvm;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
 
+import java.util.List;
+
+import es.dmoral.toasty.Toasty;
+
 public class MainActivity extends AppCompatActivity {
+
+    private NoteViewModel noteViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //create noteviewmodel
+        noteViewModel = new ViewModelProvider(this).get(NoteViewModel.class);
+        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(List<Note> notes) {
+                Toasty.success(MainActivity.this, "Data changed", Toasty.LENGTH_SHORT).show();
+            }
+        });
     }
 }
